b419ff6a7a6c79049df93b4e3bee69358a3a37db
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-06-11 19:11:01 +0200

    file as argument

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import sys
 import numpy as np
 import image_helpers as im
 from jpeg import compress_image
 from PIL import Image
 
 def main():
-    image = Image.open("./img/test_dice.png")
+    try:
+        filename = sys.argv[1]
+        image = Image.open(filename)
+    except:
+        print("Error opening input file")
+        exit()
+    
     rgb_im = image.convert('RGB')
     img = im.pad_image(rgb_im)
     I = np.asarray(img)
